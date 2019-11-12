d7f7de9dba55e464914a74283d1ef541d7ed470c
el PS <kompavser@yandex.com>
2019-11-13 02:43:18 +0500

    step 1: add firstTest

--- src/test/java/course_at_mobile/step1/FirstTest.java
+++ src/test/java/course_at_mobile/step1/FirstTest.java
@@ -1,4 +1,42 @@
 package course_at_mobile.step1;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
 public class FirstTest {
+
+    private AppiumDriver driver;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+
+        var pathToApk = System.getProperty("user.dir") + "/apks/org.wikipedia.apk";
+
+        var desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("deviceName", "a9pixel2");
+        desiredCapabilities.setCapability("platformVersion", "9.0");
+        desiredCapabilities.setCapability("automationName", "Appium");
+        desiredCapabilities.setCapability("appPackage", "org.wikipedia");
+        desiredCapabilities.setCapability("appActivity", ".main.MainActivity");
+        desiredCapabilities.setCapability("app", pathToApk);
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+    }
+
+    @AfterEach
+    public void TearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest() {
+        System.out.println("First test run");
+    }
 }
