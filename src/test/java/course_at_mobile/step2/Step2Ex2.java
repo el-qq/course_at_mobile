586c911a49ae50586a82213c84941c1f80238795
el PS <kompavser@yandex.com>
2019-11-19 01:41:03 +0500

    add test step2Ex2

--- src/test/java/course_at_mobile/step2/Step2Ex2.java
+++ src/test/java/course_at_mobile/step2/Step2Ex2.java
@@ -7,5 +7,18 @@ import org.openqa.selenium.By;
 
 class Step2Ex2 extends BaseTest {
 
+    @Test
+    @DisplayName("Ex2: проверяет наличие текста “Search…” в строке поиска перед вводом текста")
+    void Ex2Test() {
+        var searchArea = appWikipedia.findAndGetElement(By.id("org.wikipedia:id/search_container"));
+        searchArea.click();
+
+        var searchField = appWikipedia.findAndGetElement(By.id("org.wikipedia:id/search_src_text"));
+
+        var actualTextInSearchField = searchField.getText();
+        var expectedText = "Search…";
+
+        Assertions.assertEquals(expectedText, actualTextInSearchField, "В поиске поиска другой текст: " + actualTextInSearchField);
+    }
 
 }
