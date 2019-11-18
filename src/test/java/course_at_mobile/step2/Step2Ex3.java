231795e933105b4cdcd2287b866696a4ed4a3897
el PS <kompavser@yandex.com>
2019-11-19 01:41:36 +0500

    add test step2Ex3

--- src/test/java/course_at_mobile/step2/Step2Ex3.java
+++ src/test/java/course_at_mobile/step2/Step2Ex3.java
@@ -7,5 +7,32 @@ import org.openqa.selenium.By;
 
 class Step2Ex3 extends BaseTest {
 
+    @Test
+    @DisplayName("Ex3: отмена поиска")
+    void Ex3Test() {
+
+        var expectedWordForSearch = "Odyssey";
+        var byFieldResult = By.id("org.wikipedia:id/page_list_item_container");
+
+        var searchArea = appWikipedia.findAndGetElement(By.id("org.wikipedia:id/search_container"));
+        searchArea.click();
+
+        var searchField = appWikipedia.findAndGetElement(By.id("org.wikipedia:id/search_src_text"));
+        searchField.sendKeys(expectedWordForSearch);
+
+        // Ждем появления хотя бы одного результата поиска
+        appWikipedia.waitForElementPresent(byFieldResult);
+
+        // Убеждаемся, что найдено несколько статей
+        var countResult = appWikipedia.appiumDriver.findElements(byFieldResult).size();
+        if (countResult == 0) Assertions.fail("Нет результатов поиска по слову: " + expectedWordForSearch);
+
+        // Отменяем поиск
+        var cancelButton = appWikipedia.findAndGetElement(By.id("org.wikipedia:id/search_close_btn"));
+        cancelButton.click();
+
+        // Убеждаемся, что результат поиска пропал
+        appWikipedia.waitForElementNotPresent(byFieldResult);
+    }
 
 }
