b6c8597b4362a908d00095c7dec052097a25f23d
el PS <kompavser@yandex.com>
2019-11-28 10:24:04 +0500

    add test step3ex6

--- src/test/java/course_at_mobile/step3/Step3Ex6.java
+++ src/test/java/course_at_mobile/step3/Step3Ex6.java
@@ -8,4 +8,29 @@ import org.openqa.selenium.NoSuchElementException;
 
 class Step3Ex6 extends BaseTest {
 
+    @Test()
+    @DisplayName("Ex6: assert title")
+    void step2Ex6() {
+        var wordForSearch = "Homer";
+
+        appWikipedia.mainScreen.sendTextToSearch(wordForSearch);
+        var listResult = appWikipedia.searchScreen.getListWebElementsResult();
+
+        var elementTwoResult = listResult.get(1);
+        elementTwoResult.click();
+
+        //  Важно: тест не должен дожидаться появления title
+        var titleArticleBy = By.id("");
+        assertElementPresent(titleArticleBy);
+
+    }
+
+    private void assertElementPresent(By elementBy) {
+        try {
+            appiumDriver.findElement(By.id("org.wikipedia:id/view_page_title_text"));
+        } catch (NoSuchElementException err) {
+            Assertions.fail("Title у статьи НЕ нашелся");
+        }
+    }
+
 }
