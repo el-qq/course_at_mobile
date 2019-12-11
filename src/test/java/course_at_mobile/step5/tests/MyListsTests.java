f3b7e23e8e631691242d7d9947d7ccd4b9cd33c4
el-ps <kompavser@yandex.com>
2019-12-12 02:48:00 +0500

    add files before step5

---
+++ src/test/java/course_at_mobile/step5/tests/MyListsTests.java
@@ -0,0 +1,59 @@
+package course_at_mobile.step5.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MyListsTests extends BaseTest {
+
+    private static final String NAME_LISTS = "For testing";
+
+    @Test()
+    @DisplayName("Ex5: сохранение двух статей")
+    void testSaveTwoArticle() {
+        var expectedWordForSearch = "Odyssey";
+
+        var searchScreen = appMainScreen.clickSearchField();
+        searchScreen.setTextForSearch(expectedWordForSearch);
+
+        var listResult = searchScreen.getListWebElementsResult();
+        if (listResult.size() < 2) throw new AssertionError("Результат поиска состоит из менее 2 статей");
+
+        // Запоминаем первую и вторую ссылку
+        var titleFirst = listResult.get(0).getText();
+        var titleTwo = listResult.get(1).getText();
+
+        var webElementFirst = searchScreen.findByTextAndReturnWebElement(titleFirst);
+
+        // Добавляем первый результат в список
+        searchScreen.openMenuToElement(webElementFirst);
+        searchScreen.clickAddToReadingList();
+
+        // Но перед этим создаем новый список
+        searchScreen.clickGotIt();
+        searchScreen.setNewNameListAndClickOk(NAME_LISTS);
+
+        // добавляем вторую запись в список
+        var webElementTwo = searchScreen.findByTextAndReturnWebElement(titleTwo);
+        searchScreen.longTapToElement(webElementTwo);
+        searchScreen.clickAddToReadingList();
+        searchScreen.clickSaveToReadingList(NAME_LISTS);
+
+        var mainScreen = searchScreen.clickBlackAndReturmMainScreen();
+        var listsScreen = mainScreen.menuNavigation.goToMyLists();
+        listsScreen.clickListByName(NAME_LISTS);
+
+        // удаляем вторую запись
+        listsScreen.deleteLinkFromListDoubleTap(titleTwo);
+
+        var listRecords = listsScreen.getListNameRecords();
+        Assertions.assertEquals(listRecords.size(), 1, "Количество записей после удаления больше 1. Ожидается, что будет 1");
+
+        // Открываем первую статью и убеждаемся, что title совпадает
+        var articleScreen = listsScreen.clickAndOpenArticleByName(titleFirst);
+        var actualTitle = articleScreen.getNameTitle();
+        Assertions.assertEquals(actualTitle, titleFirst, "После удаления в списке отображается другая запись");
+
+    }
+
+}
---
+++ src/test/java/course_at_mobile/step5/tests/SearchTests.java
@@ -0,0 +1,62 @@
+package course_at_mobile.step5.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SearchTests extends BaseTest {
+
+    @Test
+    @DisplayName("Ex3: отмена поиска")
+    void testCancelSearch() {
+
+        var expectedWordForSearch = "Odyssey";
+
+        var searchScreen = appMainScreen.clickSearchField();
+        searchScreen.setTextForSearch(expectedWordForSearch);
+
+        // Убеждаемся, что найдено несколько статей
+        var countResult = searchScreen.getListWebElementsResult().size();
+        if (countResult == 0) Assertions.fail("Нет результатов поиска по слову: " + expectedWordForSearch);
+
+        // Отменяем поиск
+        var mainScreen = searchScreen.clickCancelSearchAndReturmMainScreen();
+
+        // Убеждаемся, что результат поиска пропал
+        var check = mainScreen.checkNotPresentSearchResult();
+        Assertions.assertTrue(check, "Результаты поиска все еще показываются на экране");
+    }
+
+    @Test
+    @DisplayName("Ex9: Рефакторинг темплейта")
+    void testCheckSearchResult3ByTitleAndDescription() {
+
+        var expectedWordForSearch = "Sagan";
+
+        var expectedTitle_1 = "Sagan";
+        var expectedDescription_1 = "Wikimedia disambiguation page";
+
+        var expectedTitle_2 = "Sagan Planet Walk";
+        var expectedDescription_2 = "Solar System scale model";
+
+        var expectedTitle_3 = "Sagan Tosu";
+        var expectedDescription_3 = "Association football club";
+
+        var searchScreen = appMainScreen.clickSearchField();
+        searchScreen.setTextForSearch(expectedWordForSearch);
+
+        // Ожидать, что найдется первый результат
+        searchScreen.waitForElementByTitleAndDescription(expectedTitle_1, expectedDescription_1);
+
+        // Написать тест, который будет делать поиск по любому запросу на ваш выбор (поиск по этому слову должен возвращать как минимум 3 результата).
+        var countResult = searchScreen.getListWebElementsResult().size();
+        if (countResult < 3) throw new AssertionError("Результат поиска содержит меннее 3 статей");
+
+        // Далее тест должен убеждаться, что первых три результата присутствуют в результате поиска
+        searchScreen.checkTitleAndDescriptionOfTheNumberElementResult(1, expectedTitle_1, expectedDescription_1);
+        searchScreen.checkTitleAndDescriptionOfTheNumberElementResult(2, expectedTitle_2, expectedDescription_2);
+        searchScreen.checkTitleAndDescriptionOfTheNumberElementResult(3, expectedTitle_3, expectedDescription_3);
+
+    }
+
+}
