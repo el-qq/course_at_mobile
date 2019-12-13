90e8f31e99aa562aea3b55b9b78299dd916c81ba
el-ps <kompavser@yandex.com>
2019-12-13 18:41:19 +0500

    ex10
    add support IOS

--- src/test/java/course_at_mobile/step5/tests/BaseTest.java
+++ src/test/java/course_at_mobile/step5/tests/BaseTest.java
@@ -1,43 +1,30 @@
 package course_at_mobile.step5.tests;
 
+import course_at_mobile.step5.helpers.AppiumHelper;
 import course_at_mobile.step5.screens.AppMainScreen;
-import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.AppiumDriver;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.net.URL;
 
 class BaseTest {
 
     AppMainScreen appMainScreen;
-    AndroidDriver appiumDriver;
+    AppiumDriver appiumDriver;
 
     @BeforeEach
-    void setUp() throws Exception {
+    void setUp() {
 
-        var pathToApk = System.getProperty("user.dir") + "/apks/org.wikipedia.apk";
+        appiumDriver = AppiumHelper.getAppiumDriverByPlatform();
 
-        var desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("deviceName", "a9pixel2");
-        desiredCapabilities.setCapability("platformVersion", "9.0");
-        desiredCapabilities.setCapability("automationName", "Appium");
-        desiredCapabilities.setCapability("appPackage", "org.wikipedia");
-        desiredCapabilities.setCapability("appActivity", ".main.MainActivity");
-        //desiredCapabilities.setCapability("app", pathToApk);
-
-        appiumDriver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        if (appiumDriver.getOrientation() == ScreenOrientation.LANDSCAPE) {
+            appiumDriver.rotate(ScreenOrientation.PORTRAIT);
+        }
         appMainScreen = new AppMainScreen(appiumDriver);
     }
 
     @AfterEach
     void TearDown() {
-        if (appiumDriver.getOrientation() == ScreenOrientation.LANDSCAPE) {
-            appiumDriver.rotate(ScreenOrientation.PORTRAIT);
-        }
-
         appiumDriver.quit();
     }
 
