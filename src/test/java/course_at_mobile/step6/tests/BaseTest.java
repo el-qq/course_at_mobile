3e99e4084f5e170ae62ce151886d282b0b5b4d43
el-ps <kompavser@yandex.com>
2019-12-16 20:46:49 +0500

    before step6

---
+++ src/test/java/course_at_mobile/step6/tests/BaseTest.java
@@ -0,0 +1,31 @@
+package course_at_mobile.step6.tests;
+
+import course_at_mobile.step6.helpers.AppiumHelper;
+import course_at_mobile.step6.screens.AppMainScreen;
+import io.appium.java_client.AppiumDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.ScreenOrientation;
+
+class BaseTest {
+
+    AppMainScreen appMainScreen;
+    AppiumDriver appiumDriver;
+
+    @BeforeEach
+    void setUp() {
+
+        appiumDriver = AppiumHelper.getAppiumDriverByPlatform();
+
+        if (appiumDriver.getOrientation() == ScreenOrientation.LANDSCAPE) {
+            appiumDriver.rotate(ScreenOrientation.PORTRAIT);
+        }
+        appMainScreen = new AppMainScreen(appiumDriver);
+    }
+
+    @AfterEach
+    void TearDown() {
+        appiumDriver.quit();
+    }
+
+}
