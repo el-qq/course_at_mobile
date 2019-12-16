3eb64ce4b86c6c9946346d1d46c171666687b145
el-ps <kompavser@yandex.com>
2019-12-16 20:46:49 +0500

    before step6

---
+++ src/test/java/course_at_mobile/step6/tests/ArticleTests.java
@@ -0,0 +1,24 @@
+package course_at_mobile.step6.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ArticleTests extends BaseTest {
+
+    @Test()
+    @DisplayName("Ex6: проверка title у статьи")
+    void testCheckTitleArticle() {
+        var wordForSearch = "Homer";
+
+        var searchScreen = appMainScreen.clickSearchField();
+        searchScreen.setTextForSearch(wordForSearch);
+
+        var articleScreen = searchScreen.openArticleByNumberResult(2);
+
+        //  Важно: тест не должен дожидаться появления title
+        var checkTitle = articleScreen.findAndCheckTitle();
+        Assertions.assertTrue(checkTitle, "У статьи НЕ нашелся заголовок (title)");
+    }
+
+}
